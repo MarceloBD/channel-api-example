5ee7fab7f46f4cb09d2db231cfffb6a959410b5c
Marcelo <marcelodianib@gmail.com>
2020-09-27 18:59:54 -0300

    add mongodb

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,29 @@
 const express = require('express')
 const cors = require('cors')
+const mongodb = require('mongodb')
 const app = express()
 const port = 3000
-
+let db = null;
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('Hello Worlds!')
-})
+const  startMongo = async () => {
+    const client = await mongodb.MongoClient.connect('mongodb://localhost:27017', {
+        useUnifiedTopology: true
+      });
+    db = client.db('channel');
+}
 
-app.get('/fruit', (req, res) => {
-    res.send([{"name": "banana", "quantity": 1}])
-  })
+startMongo();
+
+
+app.get('/', (req, res) => {
+  res.send('api')
+})  
+
+app.get('/fruit', async (req, res) => {
+    res.send(db ? await db.collection('fruit').find({}).toArray() : [])
+})
   
 
 app.listen(port, () => {
