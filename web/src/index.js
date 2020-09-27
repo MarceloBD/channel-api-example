5c5d55cf0968820623b543330a5bcc1d400045e9
Marcelo <marcelodianib@gmail.com>
2020-09-27 18:51:21 -0300

    Communicate with server

--- web/src/index.js
+++ web/src/index.js
@@ -1,5 +1,20 @@
 const React = require('react');
 const ReactDOM = require('react-dom')
+const axios = require('axios')
 
-ReactDOM.render(<div>Teste</div>, 
+const App = () =>{
+const [fruit, setFruit] = React.useState([]);
+
+
+React.useEffect(()=> {
+    axios.get('http://localhost:3000/fruit').then(res => {
+        setFruit(res.data)
+    }).catch(e => console.log(e));
+}, [])
+
+
+return <div> {fruit.map(e => <div>{e.quantity} {e.name} </div>)} </div>
+} 
+
+ReactDOM.render(<App/>, 
     document.getElementById('root'));
